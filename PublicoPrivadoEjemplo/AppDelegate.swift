ff3f2c289ff3f86efc096b18eaccedc7e94b5da2
Vanesa Korbenfeld <vkorbenfeld@gmail.com>
2022-01-18 14:22:13 -0300

    Initial commit

--- PublicoPrivadoEjemplo/AppDelegate.swift
+++ PublicoPrivadoEjemplo/AppDelegate.swift
@@ -2,7 +2,7 @@
 //  AppDelegate.swift
 //  PublicoPrivadoEjemplo
 //
-//  Created by Emiliano on 12/10/2021.
+//  Created by Vanesa Korbenfeld on 12/10/2021.
 //
 
 import UIKit
--- PublicoPrivadoEjemplo/ViewController.swift
+++ PublicoPrivadoEjemplo/ViewController.swift
@@ -2,18 +2,32 @@
 //  ViewController.swift
 //  PublicoPrivadoEjemplo
 //
-//  Created by Emiliano on 12/10/2021.
+//  Created by Vanesa Korbenfeld on 12/10/2021.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var totalVIew: UILabel!
+    @IBOutlet weak var buttonView: UIButton!
+    @IBOutlet weak var salaryView: UITextField!
+    let calculadoraDeGanancias = CalculadoraDeGanancias()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
+    @IBAction func onButtonPressed(_ sender: Any) {
+        let salary = Double(salaryView.text!)!
+        let resultado = calculadoraDeGanancias.descontarGanancias(salary: salary)
 
+        totalVIew.text = String(resultado)
+    }
 }
 
+class CalculadoraDeGanancias {
+    func descontarGanancias(salary: Double) -> Double {
+        return salary * 0.80
+    }
+}
